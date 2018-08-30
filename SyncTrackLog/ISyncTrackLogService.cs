d7ced317c77c023d44602c1e00879fc756af1833
hiteshri.y <hiteshri.y1710@gmail.com>
2018-08-30 18:18:17 +0530

    Add project files.

---
+++ SyncTrackLog/ISyncTrackLogService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SyncTrackLog
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISyncTrackLogService" in both code and config file together.
+    [ServiceContract]
+    public interface ISyncTrackLogService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/GetInterval/", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string GetInterval();
+
+        [WebInvoke(Method = "POST", UriTemplate = "/SendTrackLog", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        [OperationContract] string SendTrackLog(List<LocationLog> logs);
+
+
+    }
+
+    [DataContract]
+    
+    public class LocationLog
+    {
+        [DataMember]
+        public int IMEINo;
+        [DataMember]
+        public decimal Latitude;
+        [DataMember]
+        public decimal Longitude;
+        [DataMember]
+        public string CreatedOn;
+
+
+
+
+    }
+
+}
---
+++ SyncTrackLog/SyncTrackLogService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SyncTrackLog
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SyncTrackLogService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select SyncTrackLogService.svc or SyncTrackLogService.svc.cs at the Solution Explorer and start debugging.
+    public class SyncTrackLogService : ISyncTrackLogService
+    {
+       public string GetInterval()
+        {
+            return "{LocationInterval : 40, SyncInterval : 90 }";
+
+        }
+
+        public string SendTrackLog(List<LocationLog> log)
+        {
+            return log.Count+" records inserted successfully";
+        }
+    }
+}
